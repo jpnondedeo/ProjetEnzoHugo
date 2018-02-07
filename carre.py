0e88966069055b39542335c019b7b7eb21b4b2cb
JP <jpnondedeo@gmail.com>
2018-02-07 18:17:47 +0100

    test

--- carre.py
+++ carre.py
@@ -3,4 +3,6 @@ class Carre(object): # Classe pour créer un carré
 
     def __init__(self): # Constructeur
         self.taille = 0
-        self.couleur = ""
+        self.red = 0
+        self.green = 0
+        self.red = 0
