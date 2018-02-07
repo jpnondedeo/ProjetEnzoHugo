875728cb4a01c4e01f8da04181f95460e9896346
JP <jpnondedeo@gmail.com>
2018-02-07 20:46:50 +0100

    Ajout classe carte

--- carte.py
+++ carte.py
@@ -22,16 +22,30 @@ class Carte:
     #affiche la map en fonction de la liste generer
     def afficher(self, fenetre):
         #on charge les images
-        baseBleu = pygame.image.load(images/carreBleu.png).convert_alpha()
-        baseRouge = pygame.image.load(images/carreRouge.png).convert_alpha()
-        montagne = pygame.image.load(images/carreMarron.png).convert_alpha()
-        prairie = pygame.image.load(images/carreVert.png).convert_alpha()
-        eau = pygame.image.load(images/carreEau.png).convert_alpha()
+        baseBleu = pygame.image.load(image_bleu).convert_alpha()
+        baseRouge = pygame.image.load(image_rouge).convert_alpha()
+        pont = pygame.image.load(image_marron).convert_alpha()
+        prairie = pygame.image.load(image_vert).convert_alpha()
+        eau = pygame.image.load(image_eau).convert_alpha()
 
         #on parcours la liste du niveau
         num_ligne = 0
         for ligne in self.structure:
             #on parcours ligne par ligne
             num_case = 0
-
-
+            for sprite in ligne:
+                #on calcule la position réelle en pixels
+                x = num_case * taille_sprite
+                y = num_ligne * taille_sprite
+                if sprite == 'b':                   #b = carreBleu
+                    fenetre.blit(baseBleu, (x,y))
+                elif sprite == 'r':                 #r = carreRouge
+                    fenetre.blit(baseRouge, (x,y))
+                elif sprite == 'm':                 #m = carreMarron
+                    fenetre.blit(pont, (x,y))
+                elif sprite == 'v':                 #v = carreVert
+                    fenetre.blit(prairie, (x,y))
+                elif sprite == 'e':                 #e = carreEau
+                    fenetre.blit(eau, (x,y))
+                num_case += 1
+            num_ligne += 1
---
+++ constantes.py
@@ -0,0 +1,15 @@
+#Paramètre de la fenetre
+nombre_sprite_cote = 20
+taile_sprite = 30
+cote_fenetre = nombre_sprite_cote * taile_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "Projet Python"
+image_icone = "images/iconWarrior.png"
+
+#Images du jeu
+image_bleu = "images/carreBleu.png"
+image_rouge = "images/carreRouge.png"
+image_marron = "images/carreMarron.png"
+image_vert = "images/carreVert.png"
+image_eau = "images/carreEau.png"
